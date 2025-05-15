7dfd8aee00da9b056c80f59f95ebfeceaa753b9a
Ilqar Huseynli <huseynilqarr@gmail.com>
2025-05-15 19:43:13 +0400

    Initial commit

---
+++ auth_system/urls.py
@@ -0,0 +1,17 @@
+from django.urls import path
+from .views import (
+    RegisterView, LoginView, LogoutView, PasswordChangeView,
+    ActivateAccountView, PasswordResetRequestView, PasswordResetConfirmView
+)
+
+urlpatterns = [
+    path('login/', LoginView.as_view(), name='login'),
+    path('password-change/', PasswordChangeView.as_view(), name='password-change'),
+    path('register/', RegisterView.as_view(), name='register'),
+    path('logout/', LogoutView.as_view(), name='logout'),
+    path('activate/<uidb64>/<token>/', ActivateAccountView.as_view(), name='activate'),
+
+    # Forgot password
+    path('password-reset/', PasswordResetRequestView.as_view(), name='password-reset'),
+    path('password-reset-confirm/<uidb64>/<token>/', PasswordResetConfirmView.as_view(), name='password-reset-confirm'),
+]
